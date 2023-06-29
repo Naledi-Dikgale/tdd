ed60dfaa9ab83794d047f2ba7998a1c9abc19b6c
saluumaa <salumahassan02@gmail.com>
2023-06-29 12:14:03 +0300

    Add all failed tests

--- solver.rb
+++ solver.rb
@@ -1,13 +1,13 @@
 class Solver
-    def factorial(n)
-        if n < 0
-            raise ArgumentError
-        end
-        if n == 0
-            return 1
-        end
-        return n * factorial(n-1)
-    end
+    # def factorial(n)
+    #     if n < 0
+    #         raise ArgumentError
+    #     end
+    #     if n == 0
+    #         return 1
+    #     end
+    #     return n * factorial(n-1)
+    # end
     
     # def reverse(str)
     #     return str.reverse
--- solver_spec.rb
+++ solver_spec.rb
@@ -1,43 +1,4 @@
 require_relative 'solver'
-
-# class SolverTest < Minitest::Test
-#   def setup
-#     @solver = Solver.new
-#   end
-  
-#   def test_factorial_with_positive_integer
-#     assert_equal 1, @solver.factorial(0)
-#     assert_equal 1, @solver.factorial(1)
-#     assert_equal 6, @solver.factorial(3)
-#     assert_equal 120, @solver.factorial(5)
-#     assert_equal 3628800, @solver.factorial(10)
-#   end
-  
-#   def test_factorial_with_negative_integer
-#     assert_raises ArgumentError do
-#       @solver.factorial(-5)
-#     end
-#   end
-
-  
-#   def test_fizzbuzz
-#     assert_equal "1", @solver.fizzbuzz(1)
-#     assert_equal "2", @solver.fizzbuzz(2)
-#     assert_equal "fizz", @solver.fizzbuzz(3)
-#     assert_equal "4", @solver.fizzbuzz(4)
-#     assert_equal "buzz", @solver.fizzbuzz(5)
-#     assert_equal "fizz", @solver.fizzbuzz(6)
-#     assert_equal "7", @solver.fizzbuzz(7)
-#     assert_equal "8", @solver.fizzbuzz(8)
-#     assert_equal "fizz", @solver.fizzbuzz(9)
-#     assert_equal "buzz", @solver.fizzbuzz(10)
-#     assert_equal "11", @solver.fizzbuzz(11)
-#     assert_equal "fizz", @solver.fizzbuzz(12)
-#     assert_equal "13", @solver.fizzbuzz(13)
-#     assert_equal "14", @solver.fizzbuzz(14)
-#     assert_equal "fizzbuzz", @solver.fizzbuzz(15)
-#   end
-# end
 describe Solver do
     let(:solver) { Solver.new }
 
@@ -59,5 +20,19 @@ describe Solver do
         it "returns 'cba' when given 'abc'" do
             expect(solver.reverse("abc")).to eq("cba")
         end
+    end
+
+    describe "#fizzbuzz" do
+      it "returns Fizzbuzz when given 15" do
+        expect(solver.fizzbuzz(15)).to eq("Fizzbuzz")
+      end
+
+       it "returns Fizz when given 3" do
+        expect(solver.fizzbuzz(3)).to eq("Fizz")
+      end
+
+      it "returns Buzz when given 5" do
+        expect(solver.fizzbuzz(5)).to eq("Buzz")
+      end
     end
 end
